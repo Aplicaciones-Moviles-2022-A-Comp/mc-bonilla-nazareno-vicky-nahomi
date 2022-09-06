f58ed2803529c16b550de2b098c3e810f7f81d78
Vic <vicky.bonilla@epn.edu.ec>
2022-09-05 19:31:45 -0500

    hasta crear clases basicas

---
+++ calculadoraimc/app/src/main/java/com/example/calculadoraimc/EditarRegistro.kt
@@ -0,0 +1,11 @@
+package com.example.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditarRegistro : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_editar_registro)
+    }
+}
---
+++ calculadoraimc/app/src/main/java/com/example/calculadoraimc/ResultadoIMC.kt
@@ -0,0 +1,11 @@
+package com.example.calculadoraimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResultadoIMC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado_ic)
+    }
+}
